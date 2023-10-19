57529160cd26acedacbe8ec9a684a1278b9dd109
Alzheimer Gamus <kihakuakio@gmail.com>
2023-10-19 14:55:04 +0800

    Added NavBar and News

---
+++ src/Components/News.jsx
@@ -0,0 +1,17 @@
+export default function News() {
+    return (
+        <section className="flex items-center h-12">
+            <div className="news">
+                Latest News
+            </div>
+            <div className="news-carousel">
+                <ul className="flex gap-4">
+                    <li><a href=""></a>Racing</li>
+                    <li><a href=""></a>Virtual Reality (VR) continued to be integrated into racing games, providing a more immersive experience.</li>
+                    <li><a href=""></a>Strategy</li>
+                    <li><a href=""></a>Valheim released a new patch notes.</li>
+                </ul>
+            </div>
+        </section>
+    );
+}
