fcb54d424df7919f060c2258786bd5b4278cba20
Edward <140621113+EddyingPrime@users.noreply.github.com>
2023-10-19 14:55:51 +0800

    1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ export default {
     colors: {
       primary: "#131313",
       accent: "#FFB320",
+      white: "#FFF",
+    },
+    width: {
+      width1: "617px",
+      widthbg: "1440px",
+      widthicon: "72px",
+    },
+
+    height: {
+      height1: "804px",
+      heightbg: "500px",
+      heighticon: "72px",
     },
     extend: {},
   },
