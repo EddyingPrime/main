07f1adb69eef11b7c1fea540f8ce96a112710553
Alzheimer Gamus <kihakuakio@gmail.com>
2023-10-20 10:48:15 +0800

    Updated NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,7 @@
 export default function NavBar() {
   return (
-    <nav className="flex justify-center items-center h-14">
+    <>
+    <nav className="flex justify-center items-center h-16">
       <div className="flex container justify-between items-center">
         <div className="logo">
           <a href="">GameHaven</a>
@@ -18,5 +19,19 @@ export default function NavBar() {
         </div>
       </div>
     </nav>
+    <section className="flex items-center h-12">
+      <div className="news">
+          Latest News
+      </div>
+      <div className="news-carousel">
+          <ul className="flex gap-4">
+              <li><a href=""></a>Racing</li>
+              <li><a href=""></a>Virtual Reality (VR) continued to be integrated into racing games, providing a more immersive experience.</li>
+              <li><a href=""></a>Strategy</li>
+              <li><a href=""></a>Valheim released a new patch notes.</li>
+          </ul>
+      </div>
+    </section>
+  </>
   );
 }
