bf51d587b6d39e29a6bd8011cec4d42fb68379f0
Alzheimer Gamus <kihakuakio@gmail.com>
2023-10-25 09:52:03 +0800

    Change to Carousel and Home Content

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,49 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react"
+import { ChevronLeft, ChevronRight } from "react-feather"
+import { Link } from 'react-router-dom';
 
+export default function Carousel({ 
+  children: slides, 
+  autoSlide =false, 
+  autoSlideInterval = 3000, 
+}) {
+  const [curr, setCurr] = useState(0)
 
-const images = [
-  'src/assets/Images/image.png',
-  'src/assets/Images/image2.png',
-  'src/assets/Images/image3.png',
-  'src/assets/Images/image4.png',
-  'src/assets/Images/image5.png',
-];
-
-const Carousel = () => {
-  const [currentImage, setCurrentImage] = useState(0);
+  const prev = () => setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1))
+  const next = () => setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, []);
+    if (!autoSlide) return
+    const slideInterval = setInterval(next, autoSlideInterval)
+    return () => clearInterval(slideInterval)
+  }, [])
 
   return (
-    <>
-      <main>
-        <div>
-          {images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`Image ${index}`}
-              className={`absolute transition-opacity duration-1000 ${
-                index === currentImage ? 'opacity-100' : 'opacity-0'
-              }`}
-            />
-          ))}
+      <main className="overflow-hidden relative h-[770px]">
+        <div className="flex transition-transform ease-in-out duration-1000" style={{ transform: `translateX(-${curr * 100}%)`}}>
+        {slides.map((slide, index) => (
+          <div key={index} style={{ minWidth: "100%" }}>
+            {slide}
+          </div>
+        ))}
         </div>
-        <div className='absolute rounded mt-40 p-10 z-10 bg-dark w-75 opacity-100 , tablet:opacity-60 tablet:hover:opacity-90 tablet:left-20 tablet:mt-96 tablet:pb-10'>
-          <h2 className='text-accent font-Titilium text-2xl pb-4'>THE BEST GAMES OUT THERE</h2>
-          <p className='text-default font-Titilium pb-2'>Discover tons of free games sorted from all platforms - rated by the gamers.</p><br />
-          <span className='font-Titilium text-dark text-lg bg-accent rounded-full p-2 , hover:text-default hover:p-2.5 hover:border-b-accent'><a  href="#">Browse Games</a></span>
+        <div className="absolute inset-0 flex items-center justify-between p-4">
+          <button onClick={prev} className="p-1 rounded-full text-gray-800 hover:text-accent" aria-label="Previous">
+            <ChevronLeft size={40} />
+          </button>
+          <button onClick={next} className="p-1 rounded-full text-gray-800 hover:text-accent" aria-label="Next">
+            <ChevronRight size={40} />
+          </button>
+          <div className="absolute flex ml-40 mt-96 w-fit rounded bg-dark bg-opacity-40 p-6 , hover:bg-dark hover:bg-opacity-75 hover:p-6 hover:rounded">
+            <div className="text-center">
+              <h1 className="text-accent text-4xl font-bold drop-shadow font-Titilium">EXPLORE A HUGE LIBRARY OF FREE GAMES</h1>
+              <p className="text-default text-xl mt-4"><em>"Explore and Connect. Your Gateway to Free Games and building a community."</em></p><br />
+              <Link className="text-accent font-Titilium text-xl hover:border-b" to="/games">Browse Now!</Link>
+            </div>
+          </div>
+          <div className="absolute bottom-4 right-0 left-0">
+            <div className="flex items-center justify-center gap-2">
+              {slides.map((_, i) => (
+                <div key={i} className={`transition-all w-2 h-2 bg-accent rounded-full
+                  ${curr === i ? "p-2": "bg-opacity-50"}
+                  `} />
+                ))}
+            </div>
+          </div>
         </div>
       </main>
-    </>
-  );
+  )
 };
-
-export default Carousel;
-
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,28 @@
 import NavBar from '../Components/NavBar';
 import Carousel from '../Components/Carousel';
+import demoVideo from '../assets/Images/demoVideo.mp4'
 
-
+const slides = [
+  'src/assets/Images/image.png',
+  'src/assets/Images/image2.png',
+  'src/assets/Images/image3.png',
+  'src/assets/Images/image4.png',
+  'src/assets/Images/image5.png',
+];
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <Carousel style="position: relative"/>
-      <div>This is a test sentence!</div>
+      <Carousel>
+        {[
+          <video key="video" src={demoVideo} autoPlay muted loop />,
+          ...slides.map((s, index) => <img key={index} src={s} alt={`Slide ${index}`} />),
+        ]}
+      </Carousel>
     </>
   )
-}
+};
 
 
 
