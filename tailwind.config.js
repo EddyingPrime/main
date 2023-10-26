4b68f193603c3c1f99648cf5186c7cf6179c256c
Edward <140621113+EddyingPrime@users.noreply.github.com>
2023-10-26 16:36:26 +0800

    1.5

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,62 +4,55 @@ export default {
   theme: {
     colors: {
       primary: "#131313",
-      accent: "#FFB320",
+      lgray: "#d3d3d3",
       white: "#FFF",
     },
     // Screens
     screens: {
-      'phone': '390px',
+      phone: "390px",
       // => @media (min-width: 390px) { ... }
-      'tablet': '720px',
+      tablet: "720px",
       // => @media (min-width: 640px) { ... }
 
-      'laptop': '1024px',
+      laptop: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'desktop': '1280px',
+      desktop: "1280px",
       // => @media (min-width: 1280px) { ... }
     },
-
-
 
     extend: {
       // Container Utilities
       width: {
-        'container': '1440px',
+        container: "1440px",
       },
       // Background Utilities
       backgroundColor: {
-        'dark': '#131313',
-        'accent': '#FFB320',
+        dark: "#131313",
+        accent: "#FFB320",
       },
       top: {
-        '700': '35rem',
+        700: "35rem",
       },
       // Font Color Utilities
       textColor: {
-        'default': '#FFFFFF',
-        'accent': '#FFB320',
+        default: "#FFFFFF",
+        accent: "#FFB320",
       },
       // Font Style Utilities
       fontFamily: {
-        'Titilium':['Titillium Web', 'sans-serif'],
-        'BrunoSC':['Bruno Ace SC', 'sans-serif'],
-        'Bruno':['Bruno Ace', 'sans-serif'],
+        Titilium: ["Titillium Web", "sans-serif"],
+        BrunoSC: ["Bruno Ace SC", "sans-serif"],
+        Bruno: ["Bruno Ace", "sans-serif"],
       },
       // Border Utilities
       borderColor: {
-      'accent': '#FFB320',
-      },
-      // Avatar Height
-      width: {
-        'small': '300px',
-        'medium': '500px',
+        accent: "#FFB320",
       },
       // Absolute Bottom Adjustment
       marginBottom: {
-        '19': '19px',
-      }
+        19: "19px",
+      },
     },
   },
   plugins: [],
