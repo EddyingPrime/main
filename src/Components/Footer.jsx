4e73f6cca77f3c86344cc8967d05ff33cb794b16
Alzheimer Gamus <kihakuakio@gmail.com>
2023-10-27 16:13:51 +0800

    Updated Files

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   const [name, setName] = useState('');
@@ -31,14 +32,15 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <div className='bg-image bg-cover h-[350px] flex justify-around'>
+        <div className='bg-image bg-cover h-[350px] tablet:flex tablet:justify-around'>
           <div className='w-[700px]'>
             <div>
               <h3 className='text-accent font-Bruno text-center text-[50px] px-10 pt-10'>GameHaven</h3>
               <p className="text-default font-Titilium text-center text-lg px-10 pt-4 pb-6"><em>" Your one-stop hub for updates, free game list across platforms, and engaging gaming forums - where gamers unite. "</em></p>
             </div>
             <div>
-              <ul className='flex gap-10 p-10'>
+              <ul className='flex gap-2
+              tablet:gap-10 tablet:p-10'>
                 <li><a href=""><img src="src/assets/Icons/steam-line.png" alt="" /></a></li>
                 <li><a href=""><img src="src/assets/Icons/switch-line.png" alt="" /></a></li>
                 <li><a href=""><img src="src/assets/Icons/Smartphone.png" alt="" /></a></li>
@@ -71,7 +73,17 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <div className='bg-dark h-[50px]'></div>
+        <div className='flex bg-dark h-[50px] text-default items-center justify-between px-20'>
+          <div>All Rights Reserved &copy; GameHaven 2023</div>
+          <div>
+          <ul className="flex justify-center gap-4 pt-2 text-default text-lg font-BrunoSC text-md h-9 , tablet:gap-4 tablet:pb-0 tablet:pt-2 , desktop:gap-14">
+            <li className='hover:border-b-2 hover:border-b-accent'><Link to="/">Home</Link></li>
+            <li className='hover:border-b-2 hover:border-b-accent'><Link to="/games">Games</Link></li>
+            <li className='hover:border-b-2 hover:border-b-accent'><Link to="/blog">Blog</Link></li>
+            <li className='hover:border-b-2 hover:border-b-accent'><Link to="/forums">Forums</Link></li>
+          </ul>
+        </div>
+        </div>
       </footer>
     </>
   );
